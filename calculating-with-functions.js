fb3c9a9e012fc17b4923d2968290333bccd18e99
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-02 08:48:12 +0800

    Add func: operation function able to return concatenated op value plus arg value num

--- calculating-with-functions.js
+++ calculating-with-functions.js
@@ -1,15 +1,67 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+const operation = function(number) {
+  const operator = "+";
+  return operator + number
+}
+const number = function(...operation) {
+  const value = "0";
+  let result = null;
+    if(operation) {
+      result = Function(`'use strict'; return ($value + ${operation})`)();
+    }
+    else {
+      result = value;
+    }
+  return result; 
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+const zero = function (...operation) {
+  const value = "0";
+  let result = null;
+    if(operation.length) {
+      result = Function(`'use strict'; return ($value + ${operation})`)();
+    }
+    else {
+      result = value;
+    }
+  return result; 
+}
+
+const one = function (...operation) {
+  const value = "1";
+  let result = null;
+    if(operation.length) {
+      result = Function("'use strict'; return (value + operation)")();
+    }
+    else {
+      result = value;
+    }
+  return result; 
+}
+
+const two = function (...operation) {
+  const value = "2";
+  let result = null;
+    if(operation.length) {
+      result = Function("'use strict'; return (`${value} ${operation}`)")();
+    }
+    else {
+      result = value;
+    }
+  return result; 
+}
+
+// function three() {}
+// function four() {}
+// function five() {}
+// function six() {}
+// function seven() {}
+// function eight() {}
+// function nine() {}
+
+const plus = function(number) {
+  const operator = "+";
+  return `${operator} ${number}`;
+}
+// function minus() {}
+// function times() {}
+// function dividedBy() {}
