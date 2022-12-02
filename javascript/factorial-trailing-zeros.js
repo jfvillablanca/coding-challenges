f39d2be1fe7e787b6ee6a03c14278262558c7d9c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-02 16:10:22 +0800

    Add trailing zeros for factorial kata

---
+++ javascript/factorial-trailing-zeros.js
@@ -0,0 +1,7 @@
+function zeros(n) {
+
+}
+console.log(zeros(0), 0, "Testing with n = 0");
+console.log(zeros(5), 1, "Testing with n = 5");
+console.log(zeros(6), 1, "Testing with n = 6");
+console.log(zeros(30), 7, "Testing with n = 30");
