69a775d6c4a6cdd6ee3d651e034477cca8566be5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-02 10:03:05 +0800

    Create factory functions for number func

--- calculating-with-functions.js
+++ calculating-with-functions.js
@@ -3,58 +3,23 @@ const operation = function(operationValue) {
 }
 
 const number = function(numberValue) {
-  return Function('...operation', `'use strict'; let result; if(operation.length) { result = parse(value + operation); } else { result = value; } return result; `)
+  return Function('...operation', `let result; if(operation.length) { result = parse("${numberValue}" + operation); } else { result = "${numberValue}"; } return result; `)
 }
 
 function parse(str) {
   return Function(`'use strict'; return (${str})`)()
 }
 
-const zero = function (...operation) {
-  const value = "0";
-  let result = null;
-    if(operation.length) {
-      result = parse(value + operation);
-    }
-    else {
-      result = value;
-    }
-  return result; 
-}
-
-const one = function (...operation) {
-  const value = "1";
-  let result = null;
-    if(operation.length) {
-      // result = value + operation;
-      // result = Function("'use strict'; return value + operation;")();
-      result = parse(value + operation);
-    }
-    else {
-      result = value;
-    }
-  return result; 
-}
-
-const two = function (...operation) {
-  const value = "2";
-  let result = null;
-    if(operation.length) {
-      result = parse(value + operation);
-    }
-    else {
-      result = value;
-    }
-  return result; 
-}
-
-// function three() {}
-// function four() {}
-// function five() {}
-// function six() {}
-// function seven() {}
-// function eight() {}
-// function nine() {}
+const zero = number("0");
+const one = number("1");
+const two = number("2");
+const three = number("3");
+const four = number("4");
+const five = number("5");
+const six = number("6");
+const seven = number("7");
+const eight = number("8");
+const nine = number("9");
 
 const plus = operation("+");
 const minus = operation("-");
