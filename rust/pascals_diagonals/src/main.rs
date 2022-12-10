11e6561b400e28d90c7c15486f7545dfb3379f7c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-10 15:44:29 +0800

    Add func: factorial() and binomial_coeff()

--- rust/pascals_diagonals/src/main.rs
+++ rust/pascals_diagonals/src/main.rs
@@ -1,3 +1,17 @@
+fn factorial (x: usize) -> usize {
+    if x == 0 {
+        1
+    } else {
+        let z = x * factorial(x-1);
+        println!("x {x} z {z}");
+        z
+    }
+}
+
+fn binomial_coefficient(n: u8, r: u8) -> usize {
+    factorial(n.into()) / (factorial(r.into()) * (factorial((n-r).into())))
+}
+
 // fn generate_diagonal (base: u8, l: usize) -> Vec<u64> {
 //     todo!()
 // }
