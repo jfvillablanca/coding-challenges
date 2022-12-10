a50b55f75a598bc3e92d0a081ba0afc27d1340b5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-10 16:21:38 +0800

    Add solution to pascals diagonals kata

--- rust/pascals_diagonals/src/main.rs
+++ rust/pascals_diagonals/src/main.rs
@@ -2,9 +2,7 @@ fn factorial (x: usize) -> usize {
     if x == 0 {
         1
     } else {
-        let z = x * factorial(x-1);
-        println!("x {x} z {z}");
-        z
+        x * factorial(x-1)
     }
 }
 
@@ -12,10 +10,15 @@ fn binomial_coefficient(n: u8, r: u8) -> usize {
     factorial(n.into()) / (factorial(r.into()) * (factorial((n-r).into())))
 }
 
-// fn generate_diagonal (base: u8, l: usize) -> Vec<u64> {
-//     todo!()
-// }
+fn generate_diagonal (base: u8, l: usize) -> Vec<u64> {
+    let mut diagonal: Vec<u64> = Vec::new();
+    for i in 0 .. l {
+        diagonal.push(binomial_coefficient(base+i as u8, base) as u64)
+    }
+    diagonal
+}
 
 fn main() {
-    // generate_diagonal()
+    let test = generate_diagonal(1, 10);
+    println!("test {:?}", test);
 }
