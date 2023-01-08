ba54c80479b5a4082a7918d0b83f03ef4c17de6d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-08 22:10:32 +0800

    Final solution to kata

--- javascript/ip_address_count.js
+++ javascript/ip_address_count.js
@@ -1,4 +1,15 @@
 function ipsBetween(start, end) {
-  //TODO
+  const startvalues = start.split(".");
+  const endvalues = end.split(".");
+
+  let diff = 0;
+  let j = 3;
+  for (i = 0; i < 4; i++) {
+    const curdiff = +endvalues[i] - +startvalues[i];
+    diff += curdiff * Math.pow(2, 8 * j);
+    j--;
+  }
+  return diff;
 }
+ipsBetween("10.0.0.0", "10.0.0.50");
 module.exports = ipsBetween;
