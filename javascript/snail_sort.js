8a8081a497e079762ac0c54996bba360923fa2a0
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-08 15:27:44 +0800

    Initial working solution, works on 2x2 and 3x3

--- javascript/snail_sort.js
+++ javascript/snail_sort.js
@@ -1,6 +1,44 @@
 function snail(array) {
   if (array.flat().length === 0) return [];
 
+  const flat = [];
+  let y = 0;
+  let x = 0;
+
+  const numSteps = Math.pow(array.length, 2);
+
+  let state = 0;
+  let turns = 0;
+  let stepSize = array.length - 1;
+
+  for (step = 1; step <= numSteps; step++) {
+    // console.log(`x: ${x} y: ${y} val: ${array[y][x]}`);
+    // flat.push(array[y][x]);
+    console.log(
+      `${step} state: ${state}, turns: ${turns}, stepsize: ${stepSize}`
+    );
+    switch (state) {
+      case 0:
+        x += 1;
+        break;
+      case 1:
+        y += 1;
+        break;
+      case 2:
+        x -= 1;
+        break;
+      case 3:
+        y -= 1;
+        break;
+    }
+    if (step % stepSize === 0) {
+      state = (state + 1) % 4;
+      turns++;
+      if (turns % array.length === 0) {
+        stepSize--;
+      }
+    }
+  }
   return flat;
 }
 
